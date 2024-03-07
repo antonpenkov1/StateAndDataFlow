614ee53fab9fa0fbfe0666a856d65e3be7e55275
anton_penkov <anton.penkov1@gmail.com>
2024-03-07 19:51:58 +0100

    Initial Commit

---
+++ StateAndDataFlow/ContentView/ContentView.swift
@@ -0,0 +1,67 @@
+//
+//  ContentView.swift
+//  StateAndDataFlow
+//
+//  Created by Alexey Efimov on 06.03.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @EnvironmentObject private var contentViewVM: ContentViewViewModel
+    @EnvironmentObject private var loginViewVM: LoginViewViewModel
+    
+    var body: some View {
+        VStack {
+            Text("Hi, \(loginViewVM.name)!")
+                .padding(.top, 100)
+                .font(.largeTitle)
+            Text(contentViewVM.counter.formatted())
+                .font(.largeTitle)
+                .padding(.top, 100)
+            
+            Spacer()
+            
+            ButtonView(
+                title: contentViewVM.buttonTitle,
+                color: .red,
+                action: contentViewVM.startTimer
+            )
+            
+            Spacer()
+            
+            ButtonView(title: "LogOut", color: .blue) {
+                loginViewVM.isLoggedIn.toggle()
+                loginViewVM.name = ""
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+        .environmentObject(ContentViewViewModel())
+        .environmentObject(LoginViewViewModel())
+}
+
+struct ButtonView: View {
+    let title: String
+    let color: Color
+    let action: () -> Void
+    
+    var body: some View {
+        Button(action: action) {
+            Text(title)
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundStyle(.white)
+        }
+        .frame(width: 200, height: 60)
+        .background(color)
+        .clipShape(.rect(cornerRadius: 20))
+        .overlay(
+            RoundedRectangle(cornerRadius: 20)
+                .stroke(.black, lineWidth: 4)
+        )
+    }
+}
---
+++ StateAndDataFlow/LoginView/LoginView.swift
@@ -0,0 +1,44 @@
+//
+//  LoginView.swift
+//  StateAndDataFlow
+//
+//  Created by Alexey Efimov on 06.03.2024.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    @EnvironmentObject private var loginViewVM: LoginViewViewModel
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Spacer()
+                
+                TextField("Enter your name", text: $loginViewVM.name)
+                    .multilineTextAlignment(.center)
+                
+                Spacer()
+                
+                Text(loginViewVM.name.count.formatted())
+                    .foregroundStyle(loginViewVM.name.count > 2 ? .green : .red)
+            }
+            
+            Button(action: login) {
+                Label("OK", systemImage: "checkmark.circle")
+            }
+        }
+        .padding()
+    }
+    
+    private func login() {
+        if loginViewVM.name.count > 2 {
+            loginViewVM.isLoggedIn.toggle()
+        }
+    }
+}
+
+#Preview {
+    LoginView()
+        .environmentObject(LoginViewViewModel())
+}
